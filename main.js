61dd07eb07747943a9f13160c2c701bc05420339
Bennett <drbmgit@gmail.com>
2023-05-24 19:22:53 -0400

    main js file

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+let video = document.getElementById('ytd-player');
+
+
+
+  document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate = 2.5;
+
+  // bind keystroke to speed up, speed down, reset. x .1 increments

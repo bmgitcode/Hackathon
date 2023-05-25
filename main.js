028a7af8ec4adf8d192528288414708c903b2c12
JaniceKZ <janicechu@Janice.local>
2023-05-24 21:15:39 -0400

    player error

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // const video = document.getElementsByClassName("video-stream html5-main-video");
+  const video = document.querySelector("movie_player");
 
+  video.addEventListener('keydown', (e) => {
+    if (e.code === 'g') {
+      video.playbackRate -= 0.5;
+    };
+  });
+  
+  video.addEventListener('keydown', (e) => {
+    if (e.code === 'h') {
+      video.playbackRate = 10;
+    };
+  });
 
-let video = document.getElementById('ytd-player');
-
-
-
-  document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate = 2.5;
-
+  video.addEventListener('keydown', (e) => {
+    if (e.code === 'r') {
+      video.playbackRate = 1.0;
+    };
+  });
   // bind keystroke to speed up, speed down, reset. x .1 increments
+});
